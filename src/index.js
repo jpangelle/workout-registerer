ff174c0a61592d953a3814cf3ac6b7ca7263bf3c
JP Angelle <jp@angelle.me>
2020-07-05 22:18:23 -0500

    refactor to find by 8am

--- src/index.js
+++ src/index.js
@@ -1,37 +1,27 @@
 const dotenv = require('dotenv');
 const puppeteer = require('puppeteer');
-const { DateTime } = require('luxon');
 const { login } = require('./login');
 const { sendMessage } = require('./sendMessage');
 const { logError } = require('./logError');
 
 dotenv.config();
 
-async function joinWorkout(page, signUpDate) {
+async function joinWorkout(page) {
   await page.click(
     '#AthleteTheme_wtLayoutNormal_block_wtMenu_AthleteTheme_wt67_block_wt37',
   );
 
-  // find 8:00am row under the corresponding sign up date
-  const timeRow = await page.waitForXPath(
-    `//span[text()[contains(., '${signUpDate}')]]/parent::td/parent::tr/following-sibling::tr[4]`,
+  // find registration button for first 8:00am class
+  const reservationButton = await page.waitForXPath(
+    `//span[@title="8:00 AM CrossFit"]/../../..//a[@title="Make Reservation"]`,
   );
 
-  const time = await page.evaluate(
-    element => element.children[0].innerText,
-    timeRow,
-  );
+  await reservationButton.click();
 
-  if (time === '8:00 AM CrossFit') {
-    await timeRow.click('td:nth-child(3) > div > a');
-    await page.waitForXPath(
-      `//*[text()[contains(., 'Reservation Confirmed')]]`,
-    );
-  }
+  await page.waitForXPath(`//*[text()[contains(., 'Reservation Confirmed')]]`);
 }
 
 async function signUpForWorkout() {
-  const signUpDate = DateTime.local().plus({ days: 2 }).toFormat('MM/dd/yyyy');
   try {
     const browser = await puppeteer.launch({
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
@@ -47,8 +37,8 @@ async function signUpForWorkout() {
     );
     await login(page);
     try {
-      await joinWorkout(page, signUpDate);
-      const message = `You have been signed up for the 8:00 AM workout on ${signUpDate}`;
+      await joinWorkout(page);
+      const message = `You have been signed up for the 8:00 AM workout.`;
       await sendMessage(message);
     } catch (error) {
       logError({
